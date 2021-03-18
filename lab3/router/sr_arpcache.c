40c81989b18be603fcf0f89318734c2c5dd9a15a
Hohungduy <duyhohung.work@gmail.com>
2021-03-17 21:22:34 -0700

    nothing

--- lab3/router/sr_arpcache.c
+++ lab3/router/sr_arpcache.c
@@ -17,7 +17,7 @@
   See the comments in the header file for an idea of what it should look like.
 */
 void sr_arpcache_sweepreqs(struct sr_instance *sr) { 
-    /* Fill this in */
+    /* Fill this in -ok */
 }
 
 /* You should not need to touch the rest of this code. */
